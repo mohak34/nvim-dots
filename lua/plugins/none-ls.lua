f4021066bbab720b9972680c45cddda72bf7b5bd
Mohak S <mohaks0408@gmail.com>
2024-06-20 21:53:30 +0530

    Fixed bugs

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -6,8 +6,8 @@ return {
       sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier,
-        null_ls.builtins.diagnostics.erb_lint,
-              },
+        -- null_ls.builtins.diagnostics.erb_lint,
+      },
     })
 
     vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
