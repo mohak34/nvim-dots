5e53c5cbcf37353ccbec62fd531a27a9dba9b2d4
Mohak <mohaks0408@gmail.com>
2024-06-20 14:38:40 +0530

    Fixed formatting bug

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,6 +7,7 @@ return {
 		"MunifTanjim/nui.nvim",
 	},
 	config = function()
-		vim.keymap.set("n", "<c-n>", ":Neotree filesystem reveal left<CR>", {})
+		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
+	--	vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
 	end,
 }
